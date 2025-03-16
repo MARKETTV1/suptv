da7293d66a614701460e7d858f5216da2ecc944f
MARKETTV1 <aliridakarim8@gmail.com>
2025-03-16 14:01:09 +0000

    Update suptv.sh

--- suptv.sh
+++ suptv.sh
@@ -6,7 +6,7 @@ opkg remove enigma2-plugin-extensions-suptv
 wget -O /tmp/enigma2-plugin-extensions-suptv_4.3_all.ipk http://team4e2.xyz/plugins/enigma2-plugin-extensions-suptv_4.3_all.ipk
 
 # Installation du fichier .ipk
-opkg install /tmp/enigma2-plugin-extensions-suptv_4.1_all.ipk
+opkg install /tmp/enigma2-plugin-extensions-suptv_4.3_all.ipk
 
 # Optionnel: Redémarrer Enigma2 ou recharger le service
 sleep 3
